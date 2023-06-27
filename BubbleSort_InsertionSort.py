e5300a752544aeee4650040755ce96a823263bb7
UsernameAnda <caratme02@gmail.com>
2023-06-27 03:21:09 -0700

    Initial commit

---
+++ BubbleSort_InsertionSort.py
@@ -0,0 +1,59 @@
+#DIAN ULI GUSTIANI NGKALE
+#F55121014
+
+import time
+
+def bubble_sort(arr):
+    n = len(arr)
+    start_time = time.time()
+    for i in range(n - 1):
+        for j in range(n - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+        if i < 5 or i >= n - 5:
+            print(f"Iterasi {i + 1}: {arr}")
+    end_time = time.time()
+    print("Array yang diurutkan (Bubble Sort):", arr)
+    print("Waktu perhitungan Bubble Sort:", end_time - start_time)
+
+def insertion_sort(arr):
+    n = len(arr)
+    start_time = time.time()
+    for i in range(1, n):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and arr[j] > key:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+        if i < 5 or i >= n - 5:
+            print(f"Iterasi {i}: {arr}")
+    end_time = time.time()
+    print("Array yang diurutkan (Insertion Sort):", arr)
+    print("Waktu perhitungan insertion Sort:", end_time - start_time)
+
+def main():
+    arr = [12, 99, 62, 15, 20, 95, 39, 48, 3, 24, 8, 43, 74, 19, 97, 33, 49, 68, 55, 33, 90, 90, 7,
+           26, 85, 46, 39, 40, 9, 36, 60, 64, 89, 31, 25, 71, 21, 23, 63, 84, 32, 5, 3, 44, 21, 10, 21,
+           17, 50, 2, 36, 53, 79, 54, 19, 88, 1, 32, 31, 15, 6, 3, 1, 40, 22, 43, 18, 1, 77, 9, 59,
+           40, 7, 41, 81]
+
+    print("Array yang belum diurutkan:", arr)
+
+    for _ in range(2):
+        choice = input
+        while True:
+            choice = input("Masukkan 'B' untuk Bubble Sort atau 'I' untuk Insertion Sort: ")
+            if choice.upper() == 'B':
+                bubble_sort(arr.copy())
+                break
+            elif choice.upper() == 'I':
+                insertion_sort(arr.copy())
+                break
+            else:
+                print("Pilihan tidak sah. Coba lagi.")
+
+        print()
+
+if __name__ == '__main__':
+    main()
